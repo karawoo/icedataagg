dffdb4ff2837262e263e6b4ab15a4a780c546aa1
karawoo <woo.kara@gmail.com>
2014-09-25 16:38:15 -0700

    Add some lake metadata. See #1

---
+++ R/lakemeta.R
@@ -0,0 +1,25 @@
+lakemeta <- data.frame(
+    LakeName = "Lake Baikal",
+    LakeRegLoc = "Siberia",
+    LakeCountry = "Russia",
+    LakeArea = 31722,
+    LakeMeanDepth = "insert mean depth here",
+    LakeMaxDepth = 1642,
+    LakeElevation = 455.5,
+    Watershed = "find this out",
+    H2OResidence = (330 * 365.25),
+    LakeMaxFetch = 636,
+    stringsAsFactors = FALSE
+)
+
+
+### Resources
+
+### area, max depth, elevation:
+### http://users.ugent.be/~mdbatist/intas/morphometry.htm
+
+### residence time:
+### http://lin.irk.ru/grachev/eng/introduction.htm
+
+### fetch:
+### http://www.tahoebaikal.org/about/lake-tahoe-and-lake-baikal-watersheds/
