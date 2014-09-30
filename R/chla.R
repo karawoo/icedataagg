752fd8ed38510fc2e16b9a87cfa41c8b7edc7325
karawoo <woo.kara@gmail.com>
2014-09-30 15:05:25 -0700

    file to aggregate chlorophyll a data

---
+++ R/chla.R
@@ -0,0 +1,16 @@
+library('lubridate')
+
+source("datadir.R")
+source("depths.R")
+source("dates.R")
+
+chla <- read.csv(paste0(datadir, "Longterm_data/temp_chl_secchi_wind/cleaned_data/chla_cleaned.csv"), stringsAsFactors = FALSE)
+
+chla_dates <- merge(chla, winterdates, by.x = "Year", by.y = "iceoff_year",
+                    all.x = TRUE, all.y = FALSE)
+
+chla_dates <- chla_dates %>%
+  filter(iceon <= as.Date(date) & as.Date(date) <= iceoff 
+         | month(date) %in% c(7, 8, 9)) %>%
+  mutate(season = ifelse(month(date) %in% c(7, 8, 9), "summer", "winter")) %>%
+  arrange(date, depth)
