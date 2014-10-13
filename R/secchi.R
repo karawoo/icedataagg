6f8a90f15485c6f9d96705d5346ea557a054c682
karawoo <woo.kara@gmail.com>
2014-10-13 16:51:44 -0700

    aggregate secchi data

---
+++ R/secchi.R
@@ -0,0 +1,21 @@
+library('lubridate')
+library('dplyr')
+
+source("R/datadir.R")
+source("R/datesdepths.R")
+
+secchi_dates <- data.frame(date = as.Date(unique(secchi$date))) %>%
+  filter(sapply(date, date_subset, winterints) | month(date) %in% c(7, 8, 9))
+
+secchi_agg <- secchi %>%
+  do(na.omit(.)) %>%
+  semi_join(secchi_dates, by = "date") %>%
+  mutate(season = ifelse(month(date) %in% c(7, 8, 9), "summer", "winter")) %>%
+  mutate(photic_zone = pz(secchi_depth)) %>%
+  group_by(year, season) %>%
+  summarize(avg_secchi = mean(secchi_depth, na.rm = TRUE), 
+            avg_photic = mean(photic_zone, na.rm = TRUE), 
+            ndates = length(unique(date)), 
+            mindate = min(date), 
+            maxdate = max(date)) %>%
+  arrange(year, desc(season))
