391a586b884102616064ce562109cf9fa89d74af
karawoo <woo.kara@gmail.com>
2014-10-14 16:47:25 -0700

    remove ice duration from stmeta

--- R/stmeta.R
+++ R/stmeta.R
@@ -2,6 +2,5 @@ stmeta <- data.frame(
     StationDistance = 2.2,
     StationDepth = 800,
     StationLat = 51.88,
-    StationLong = 105.083889,
-    Ice.Duration = "find this out"
+    StationLong = 105.083889
 )
