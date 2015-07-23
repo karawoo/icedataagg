303625d82351c1248739a029f126e9b8be56ebd2
Kara Woo <woo.kara@gmail.com>
2015-07-23 16:53:13 -0700

    Remove the many old files.

--- R/co_var.R
+++
@@ -1,7 +0,0 @@
-#####################################################
-####  Function to find coefficient of variation  ####
-#####################################################
-
-co_var <- function(x, na.rm = TRUE) {
-  sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
-}
